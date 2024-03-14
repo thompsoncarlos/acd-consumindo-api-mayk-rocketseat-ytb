4e47eef47af3df1c48b706c21aac228ef42761ef
Carlos Thompson <carlosethompson@gmail.com>
2024-03-14 15:55:20 +0000

    feat: consuming a api with fetch and axios

---
+++ server.js
@@ -0,0 +1,23 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const axios = require ('axios');
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+
+  try {
+    // response é a resposta do axios mas eu tiro o data de dentro do response
+    const { data } = await axios('https://jsonplaceholder.typicode.com/users');
+    return res.json(data);
+  } catch(error) {
+    console.error(error);
+  }
+
+  // return res.json([
+  //   { name: 'Carlos' },
+  //   { name: 'Eduardo' }
+  // ])
+})
+app.listen('4567');
